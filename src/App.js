92aa5b6a66e4addcde5ec01cda44dff3652cca40
MuhammadZohaib07 <66253658+MuhammadZohaib07@users.noreply.github.com>
2020-06-15 22:59:52 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import {Message} from './message.js';
+import './App.css';
+
+export default function App() {
+  let [count, setCount] = useState(1);
+  let [isMorning, setMorning] = useState(false)
+
+  return (
+    <div className={"box ${isMorning ? 'dayLight': ''}"}>
+
+      <h1>Good {isMorning ? 'Morning' : 'Night'}</h1>
+
+      <Message counter={count}/>
+      <br />
+
+      <button onClick={() => setCount(count + 1)}>
+        tasbeeh counter
+      </button>
+
+      <hr/>
+
+      <button onClick={()=>setMorning(!isMorning)}>
+        update Day
+      </button>
+
+    </div>
+
+  );
+}
+
---
+++ src/message.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Message (props) {
+return (
+    <h3>the value of counter is: {props.counter}</h3>
+)
+}
